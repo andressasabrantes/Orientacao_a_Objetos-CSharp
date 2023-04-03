dc872796ebb5938206ae7d5e2d314c71ca432eb8
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-03 15:25:00 -0300

    UdemyNelio: Primeiros Exercícios (Classes, objetos e atributos)

---
+++ Exercicios.UdemyNelio/Exercicios.UdemyNelio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios.UdemyNelio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1, pessoa2;
+
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+            Console.Write("Digite o nome da primeira pessoa: ");
+            pessoa1.nome = Console.ReadLine();
+            Console.Write("Digite a idade dessa pessoa: ");
+            pessoa1.idade = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite o nome da segunda pessoa: ");
+            pessoa2.nome = Console.ReadLine();
+            Console.Write("Digite a idade dessa pessoa: ");
+            pessoa2.idade = int.Parse(Console.ReadLine());
+
+
+            if (pessoa1.idade > pessoa2.idade) 
+            {
+                Console.WriteLine("Pessoa mais velha: {0}", pessoa1.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: {0}", pessoa2.nome);
+            }
+
+        }
+    }
+}
