99560e156451a670e9fa2ab48b09af7e0e86cd3e
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-03 14:49:31 -0300

    UdemyNelio: Criando uma classe com três atributos para representar melhor um triângulo

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de X: {0}", areaX);
+            Console.WriteLine("Área de Y: {0}", areaY);
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ConsoleApp2
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
