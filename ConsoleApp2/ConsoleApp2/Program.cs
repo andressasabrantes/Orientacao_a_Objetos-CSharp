a326c82ac9b8a3c3ef912d81ad51a3eb25d4a8e1
Andressa Abrantes <andressasabrantes@gmail.com>
2023-04-03 20:45:39 -0300

    UdemyNelio: Criando uma classe e atributos para representar melhor um triângulo

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -24,12 +24,10 @@ namespace ConsoleApp2
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             double areaX = x.Area();
-            
+
             double areaY = y.Area();
 
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
-
+            
             Console.WriteLine("Área de X: {0}", areaX);
             Console.WriteLine("Área de Y: {0}", areaY);
 
