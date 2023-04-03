12df95f2b161070a66d0a586c1afdf66ca8603da
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-03 15:26:22 -0300

    UdemyNelio: Primeros Exercícios (Classes, Objetos e atributos)

---
+++ Exercicios.UdemyNelio2/Exercicios.UdemyNelio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios.UdemyNelio2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Funcionario funcionario1, funcionario2;
+            
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
+
+            Console.Write("Digite o nome do primeiro funcionário: ");
+            funcionario1.nome = Console.ReadLine();
+            Console.Write("Digite o salário desse funcionário: ");
+            funcionario1.salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do primeiro funcionário: ");
+            funcionario2.nome = Console.ReadLine();
+            Console.Write("Digite o salário desse funcionário: ");
+            funcionario2.salario = double.Parse(Console.ReadLine());
+
+            double salarioMedio = (funcionario1.salario + funcionario2.salario) / 2.0;
+            Console.WriteLine("Salário médio = {0}", salarioMedio);
+
+        }
+    }
+}
