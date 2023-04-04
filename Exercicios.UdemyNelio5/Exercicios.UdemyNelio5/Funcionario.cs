26c527807a12869f57c9e3ae7601ab739ac68379
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-04 14:45:47 -0300

    UdemyNelio: Ler os dados de um funcionário e fazer cálculos de salário

---
+++ Exercicios.UdemyNelio5/Exercicios.UdemyNelio5/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.UdemyNelio5
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public double AumentarSalario(double porcentagem)
+        {
+            double salarioAumento = (porcentagem / 100) * SalarioBruto;
+            salarioAumento += SalarioLiquido();
+            return salarioAumento;
+        }
+    }
+}
---
+++ Exercicios.UdemyNelio5/Exercicios.UdemyNelio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios.UdemyNelio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.WriteLine("Entre com os dados do funcionário: ");
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {f.Nome}, ${f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagemSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            porcentagemSalario = f.AumentarSalario(porcentagemSalario);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {f.Nome}, ${porcentagemSalario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
