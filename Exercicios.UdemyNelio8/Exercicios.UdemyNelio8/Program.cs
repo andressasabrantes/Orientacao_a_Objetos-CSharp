81c13386e12f4d3f4645771967105d92a4c10296
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-04 15:48:14 -0300

    UdemyNelio: Exercicio8

---
+++ Exercicios.UdemyNelio8/Exercicios.UdemyNelio8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios.UdemyNelio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
