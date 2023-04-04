00388b5000f40ffd919704bae908ce73fc37607f
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-04 10:19:34 -0300

    UdemyNelio: Atualizando estoque

---
+++ Exercicios.UdemyNelio3/Exercicios.UdemyNelio3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios.UdemyNelio3 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionados: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+            int qtdRemovida = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtdRemovida);
+            Console.Write("Dados atualizados: " + p);
+
+            Console.WriteLine();
+
+        }
+    }
+}
