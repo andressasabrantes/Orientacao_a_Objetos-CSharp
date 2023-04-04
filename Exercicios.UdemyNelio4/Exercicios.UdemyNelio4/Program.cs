a4a966ccd1198eff7aa5667ed506800b4e634e65
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-04 12:06:40 -0300

    UdemyNelio: Calcular Area, Perimetro e Diagonal de um retangulo

---
+++ Exercicios.UdemyNelio4/Exercicios.UdemyNelio4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Exercicios.UdemyNelio4;
+using System;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace Exercicios.UdemyNelio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Retangulo R = new Retangulo();
+            
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            Console.Write("Largura: ");
+            R.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Altura: ");
+            R.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            double areaRetangulo = R.Area();
+            Console.WriteLine("ÁREA = " + areaRetangulo.ToString("F2", CultureInfo.InvariantCulture));
+
+            double perimetroRetangulo = R.Perimetro();
+            Console.WriteLine("PERÍMETRO = " + perimetroRetangulo.ToString("F2", CultureInfo.InvariantCulture));
+
+            double diagonalRetangulo = R.Diagonal();
+            Console.WriteLine("DIAGONAL = " + diagonalRetangulo.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+        }   
+    }
+}
---
+++ Exercicios.UdemyNelio4/Exercicios.UdemyNelio4/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.UdemyNelio4
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2;
+        }
+
+        public double Diagonal()
+        {
+            double larguraAoQuadrado = Math.Pow(Largura, 2);
+            double alturaAoQuadrado = Math.Pow(Altura, 2);
+            return Math.Sqrt(larguraAoQuadrado + alturaAoQuadrado);
+        }
+    }
+}
