74614e30bf5507a29627f2fafca5a1392facf146
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-06 17:00:45 -0300

    Main: Criação de um sistema de criação/atualização de conta bancária

---
+++ ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ContaBancaria
+{
+    internal class ContaBancaria
+    {
+        private int _numeroDaConta;
+        private string _nomeTitular;
+        private double _saldo;
+        
+     
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0)
+        {
+            _numeroDaConta = numeroConta;
+            _nomeTitular = nomeTitular;
+            _saldo = depositoInicial;
+        }
+
+        public int NumeroConta
+        {
+            get { return _numeroDaConta; }
+        }
+
+        public string NomeTitular
+        {
+            get { return _nomeTitular; }
+            set { _nomeTitular = value; }
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            _saldo += valorDeposito;
+        }
+        
+        public void Sacar(double valor)
+        {            
+            if (valor + 5 > _saldo)
+            {
+                Console.WriteLine("Erro ao sacar: Saldo Insuficiente!");
+            }
+            else
+            {
+                _saldo -= valor + 5;
+            }
+        }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine($"Número da conta: {_numeroDaConta}");
+            Console.WriteLine($"Nome do titular: {_nomeTitular}");
+            Console.WriteLine("Saldo: R$ " + _saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+    }
+
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seus dados abaixo");
+            Console.WriteLine();
+
+            Console.Write("Número da Conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome do titular: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Haverá depósito inicial? S/N ");
+            char opcao = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            ContaBancaria conta;
+
+            if (opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numeroConta, nomeTitular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numeroConta, nomeTitular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta ");
+            conta.MostrarDados();
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.Depositar(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados ");
+            conta.MostrarDados();
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            conta.Sacar(valorSaque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados ");
+            conta.MostrarDados();
+        }
+    }
+}
