d9ac060ebb92ba82560ebf92b8f937b334727b64
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-11 10:31:31 -0300

    UdemyNelio: Sistema de aluguel de quartos para estudantes

---
+++ Hostel/Hostel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hostel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] student = new Estudante[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());                                   
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}: ");
+                Console.Write("Name: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room number: ");
+                int quarto = int.Parse(Console.ReadLine());
+                student[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Occupied rooms: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (student[i] != null)
+                {
+                    Console.WriteLine(i + ": " + student[i]);
+                }
+            }
+        }
+    }
+}
