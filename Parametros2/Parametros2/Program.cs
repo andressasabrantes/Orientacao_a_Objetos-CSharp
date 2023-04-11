c05aa7a9dcb7066829f83a2733311ea195a80237
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-11 12:45:14 -0300

    UdemyNelio: Modificadores de parâmetros ref e out

---
+++ Parametros2/Parametros2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Parametros2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 40;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
