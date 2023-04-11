2fbcb384774cdf6c672c5179cd1a46869e61b2fb
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-11 12:14:14 -0300

    UdemyNelio: Modificador de Parâmetros

---
+++ Parametros/Parametros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Parametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 3);
+            int s3 = Calculator.Sum(2, 4, 3, 5, 6, 8, 10);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
