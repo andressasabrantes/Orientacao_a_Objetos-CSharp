8c8e57ebb97e46c858a4e57a4365855b1fb2ec0e
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-11 10:36:48 -0300

    Main: Sistema de aluguel de quartos para estudantes

---
+++ Hostel/Hostel/Estudante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Hostel
+{
+    internal class Estudante
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Name = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }    
+}
