ab355b46f650f77178f96547e7fe10abb11ce4cb
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-12 16:04:35 -0300

    UdemyNelio: Employee Update

---
+++ RegisteredEmployee/RegisteredEmployee/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RegisteredEmployee
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+
+        public Employee(int id, string name, double salary)
+        { 
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100.0;
+        }
+
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ RegisteredEmployee/RegisteredEmployee/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace RegisteredEmployee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 1; i <= numberOfEmployees; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int idEmployee = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string nameEmployee = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salaryEmployee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                employees.Add(new Employee(idEmployee, nameEmployee, salaryEmployee));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee i in employees)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
