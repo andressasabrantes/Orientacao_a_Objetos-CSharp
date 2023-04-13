e1ef1fa33846300a12047479626bb8205d19346c
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-13 15:42:57 -0300

    UdemyNelio: Employee System

---
+++ Course/Course/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Course.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerhour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract() 
+        { 
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerhour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerhour;
+        }
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,27 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+
+
+        }
+    }
+}
