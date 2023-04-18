3c79332a9f722e688e81bcdd4c41f3e2fd37e277
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-18 14:47:27 -0300

    UdemyNelio: Programa para ler dados de produtos e mostrar a etiqueta de preço de cada um

---
+++ ProjetoProduto/ProjetoProduto/Entities/ImportedProduct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ProjetoProduto.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price) 
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
